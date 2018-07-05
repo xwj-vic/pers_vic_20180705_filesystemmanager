a3c64dcbc0c536e3426f77a51a84e1c16bcfd3f1
XUVI7 <VIC.XU@OOCL.com>
2018-07-05 18:28:39 +0800

    asa

---
+++ src/main/java/pers/vic/fsm/DecoratorDemo/Human.java
@@ -0,0 +1,11 @@
+package pers.vic.fsm.DecoratorDemo;
+
+/**
+ * Create By Vic Xu on 7/5/2018
+ */
+public class Human extends Intelligentor {
+    @Override
+    public void walk() {
+        System.out.println("walking.....");
+    }
+}
---
+++ src/main/java/pers/vic/fsm/DecoratorDemo/Intelligentor.java
@@ -0,0 +1,9 @@
+package pers.vic.fsm.DecoratorDemo;
+
+/**
+ * Create By Vic Xu on 7/5/2018
+ */
+public abstract class Intelligentor {
+
+    public abstract void walk();
+}
